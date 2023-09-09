628fc584f01e0298527976ce782708729510fb84
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-08 17:59:30 -0700

    Adjust 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Gives if a number is positive or negative
+ * @n: Random number
+ *
+ * Description: This function tell us if the a random number is positive or not
+ * Return: A string that declare if the number is positive or not
+ */
 int main(void)
 {
 	int n;
@@ -12,15 +19,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	/* your code goes there */
 	return (0);
