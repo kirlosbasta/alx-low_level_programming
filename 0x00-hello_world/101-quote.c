ada62d5b5608a51f5984023af788f3a9dc90156b
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-11 14:53:01 -0700

    #include <stdio.h>

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the message below
+ * Return: 1
+ */
+int main() {
+	    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	        fprintf(stderr, "%s\n", message);
+		    
+		    return 1;
+}
