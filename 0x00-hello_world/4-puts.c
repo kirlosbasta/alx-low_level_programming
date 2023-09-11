38730024ca9967e4bf4240720cd0495027af3569
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-11 10:41:19 -0700

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print a string \"programming is like bulding a multilingual puzzle'
+ * Return: Exit point*/
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
