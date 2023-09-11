35a0da2cd94ff83b20f139c1522c313479394f62
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-11 10:45:41 -0700

    Adjust 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Print a string \"programming is like bulding a multilingual puzzle'
- * Return: Exit point*/
+ * Description: Print a string
+ * \"programming is like bulding a multilingual puzzle'
+ * Return: Exit point
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
