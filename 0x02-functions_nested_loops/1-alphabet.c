34276794ea4d6ca122ff2310c21b032d2525861c
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-12 15:26:36 -0700

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the alphabet in lower case followed by new line
+ * Return: Always 0
+ */
+/**
+ * print_alphabet: Prints alphabets
+ *
+ * Description: Prints the alphabets in lowercase 
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= z)
+	{
+		_putchar(c);
+		c++;
+	}
+	return (0);
+}
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
