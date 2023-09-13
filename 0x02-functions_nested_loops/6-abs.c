ff7bdfb35213ce1855e4046d7080cd3d0a2e83a3
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-12 19:27:19 -0700

    Add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+/**
+ * _abs - Absolute
+ * @n: integer
+ *
+ * Description: Give the absoulte value
+ * Return: always n
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n *= 1;
+	}
+	else
+	{
+		n *= -1;
+	}
+	return (n);
+}
