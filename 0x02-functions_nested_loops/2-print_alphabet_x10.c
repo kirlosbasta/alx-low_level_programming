0444d81f8b939a74f3fa9d6013a6501e9a84a59a
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-12 17:43:41 -0700

    Adjust 2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints alphabets
+ * print_alphabet_x10 - Prints alphabets
  *
  * Description: Prints the alphabets ten times in lowercase
  * Return: Always 0
  */
 void print_alphabet_x10(void)
 {
-	char c = 'a';
+	char c;
 	int i = 0;
 
 	while (i < 10)
 	{
+		c = 'a';
 		while (c <= 'z')
 		{
 			_putchar(c);
