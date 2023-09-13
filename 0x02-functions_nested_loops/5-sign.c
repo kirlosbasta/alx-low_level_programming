3526dc9e89c8b1d573ead72064266142f9e921da
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-12 19:04:53 -0700

    Adjust 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+int _putchar(char c);
 /**
  * print_sign - Print the sign
  * @n: number
@@ -11,7 +12,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
