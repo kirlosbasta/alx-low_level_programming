9e249b5f8404f73b148843ff29f88d63e938cd2f
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-12 19:38:41 -0700

    Add 7-print-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+/**
+ * print_last_digit - Prints last digit
+ * @n: parameter int
+ *
+ * Description: print the last digit
+ * Return: i
+ */
+int print_last_digit(int n)
+{
+	int i = n % 10;
+
+	return (i);
+}
