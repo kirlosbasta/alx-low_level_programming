18142087db62c5ae6308664b22b342f2b7089c41
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-12 18:59:43 -0700

    Add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+/**
+ * print_sign - Print the sign
+ * @n: number
+ *
+ * Description: Print the sign of the number
+ * Return: 1 if n greater than 1
+ * 0 if n equal to zero
+ * -1 if n less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
