ff231c272889e34821453a52bae3ccb90403d5bb
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-14 08:15:25 -0700

    Add 0-main.c and main.h

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Gives if a number is positive or negative
+ *
+ * Description: This function tell us if the a random number is positive or not
+ * Return: A string that declare if the number is positive or not
+ */
+void positive_or_negative(int i)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	/* your code goes there */
+	return(0);
+}
