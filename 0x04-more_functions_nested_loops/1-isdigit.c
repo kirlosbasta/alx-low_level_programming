f3738d019be70f291b9c54e6cb795906d58644aa
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-14 13:46:24 -0700

    Add 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ * _isdigit - checks if c is digit
+ * @c: integer
+ * Return: 1 if integer and 0 if not
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
