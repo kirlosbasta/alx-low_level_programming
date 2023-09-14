6fa9acfda101326ea4c5fb622f88a522c8c98620
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-14 14:26:42 -0700

    Adjust 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
+#include "main.h"
 /**
  * print_numbers - Prints numbers from 0 to 9 in ascending order
- * 
+ *
  * Return: always 0
  */
 void print_numbers(void)
 {
 	int i = 0;
 
-	while ( i < 10)
+	while (i < 10)
 	{
 		_putchar('0' + i);
+		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
