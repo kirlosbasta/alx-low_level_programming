cb6a7ab01fe7839585c16f87a6ab4ab8c4e2ff4c
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-14 19:51:00 -0700

    Add 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print the numbers from 1 to 100
+ * but replace the multible of 3 by Fizz
+ * and the multible of 5 by Buzz
+ * and the multible of both with FizzBuzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+}
