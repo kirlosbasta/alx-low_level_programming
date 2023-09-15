15bc08e14ca54ed9b40058453a2ad00be676d5ce
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-14 17:46:18 -0700

    Adjust 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,9 +8,16 @@
 void print_diagonal(int n)
 {
 	int i = 0;
+	int j;
 
 	while (n > i)
 	{
+		j = 0;
+		while (j <= i)
+		{
+			_putchar(' ');
+			j++;
+		}
 		_putchar('\\');
 		_putchar('\n');
 		i++;
