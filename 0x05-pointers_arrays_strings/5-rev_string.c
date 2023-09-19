e3aab1c1e5ddea0255d477ee083f5d4eadda3e02
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-19 11:16:22 -0700

    Adjust 5-rev-string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,47 +18,21 @@ int _strlen(char *s)
 }
 
 /**
- * print_rev - Print the string in reverse
- * @s: Pointer to string
- * Return: Length of the string
- */
-
-void print_rev(char *s)
-{
-	int len = _strlen(s);
-
-	for (len -= 1; len >= 0; len--)
-	{
-		_putchar(s[len]);
-	}
-	_putchar('\n');
-}
-
-/**
- * _puts - Print a string followed by newline
- * @str: pointer to string
- * Return: Nothing
- */
-
-void _puts(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != 0; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
-}
-
-/**
  * rev_string - printthe string and it reverse
  * @s: Poiinter to string
- * Return: Noting
+ * Return: Nothing
  */
 
 void rev_string(char *s)
 {
-	_puts(s);
-	print_rev(s);
+	int len = _strlen(s);
+	int i;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
+	}
 }
