e08f0283e973a518ade8fc46b495cc8d9a64b5bd
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-18 19:43:47 -0700

    Adjust 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - reset the variables to 98\
- * @ *n: A Pointer to the integer we want to reset
+ * reset_to_98 - reset the variables to 98
+ * @n: A Pointer to the integer we want to reset
  * Return: nothing
  */
 void reset_to_98(int *n)
