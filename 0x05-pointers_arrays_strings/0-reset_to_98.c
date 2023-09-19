e8d177deca31da1037a15e270437b73c0ad68e52
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-18 19:36:59 -0700

    Add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the variables to 98\
+ * @ *n: A Pointer to the integer we want to reset
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+This project is about pointer and arrays
+there sould be a prototype of each function in main.h 
+only allowed to use _putchar
+Happy coding
