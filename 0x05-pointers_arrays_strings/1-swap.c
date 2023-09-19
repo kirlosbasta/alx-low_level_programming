aefb8aed470b7d512c7fa2471c741b9bb22a0316
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-18 20:03:13 -0700

    Add 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two integers
+ * @a: pointer to integer 1
+ * @b: pointer to integer 2
+ * REturn: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
