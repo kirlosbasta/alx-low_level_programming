96794ac117d5fa338f146a257ede5b705c4fcf8a
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-19 13:43:54 -0700

    Add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *_strlen - count the number of string
+ * @s: pointer to string
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * puts_half - Print the second half of string
+ * @str: Pointer to string
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int len = _strlen(str);
+	for (i =0; str[i] != 0; i++)
+	{
+		if (i >= len / 2)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
