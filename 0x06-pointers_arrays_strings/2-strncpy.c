040972e192b3eddbf84f27b8f6c34c59a63d66dc
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-21 06:10:55 -0700

    Adjust 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,15 @@ char *_strncpy(char *dest, char *src, int n)
 	char *srcptr = src;
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && *srcptr != '\0'; i++)
 	{
 		*destptr = *srcptr;
 		destptr++;
 		srcptr++;
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
