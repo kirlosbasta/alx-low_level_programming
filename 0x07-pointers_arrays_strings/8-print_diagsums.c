dc93f62c7e7ac29ac5c69c0252c0ad9ebbfbe6f4
Kirlos Basta <kirlosbasta@gmail.com>
2023-09-26 16:42:24 -0700

    Add 8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,7 +4,7 @@
 /**
  * print_diagsums - print the sum of the twocdiagonals of a square matrix
  * @a: pointer to two dimensial array
- * size: the size of the square
+ * @size: the size of the square
  * Return: Nothing
  */
 
@@ -16,10 +16,10 @@ void print_diagsums(int *a, int size)
 
 	while (i < (size * size) && j > 0)
 	{
-			first += a[i];
-			second += a[j];
-			i += size + 1;
-			j -= size - 1;
+		first += a[i];
+		second += a[j];
+		i += size + 1;
+		j -= size - 1;
 	}
 	printf("%u, %u\n", first, second);
 }
