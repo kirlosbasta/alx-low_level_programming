89c81b591612c1da6d0426c48eaea9578af522df
kirlos basta <kirlosbasta@gmail.com>
2023-10-03 05:43:24 +0300

    Add 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Adds poisitive numbers
+ * @argc: The number of argument
+ * @argv: Array of pointer to char
+ * Return: return 0 if success
+ * and 1 if one of the number contain symbols thatare not digits
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
+
