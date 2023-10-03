68df68ecfeb62c10550a127614b2d8363122b316
kirlos basta <kirlosbasta@gmail.com>
2023-10-03 04:51:12 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - ptints the number of argument
  * @argc: The number of argument
  * @argv: Array of pointer to string
- * Retun: Always 0
+ * Return: Always 0
  */
 
 int main(int argc, __attribute__((unused)) char *argv[])
