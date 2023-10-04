7975f334b88f09f56ab7c74c64da7a5524290b8f
kirlos basta <kirlosbasta@gmail.com>
2023-10-04 05:51:34 +0300

    Add 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of chars
+ * and initializes it with specific char
+ * @size: the size of the array
+ * @c: The initialization char
+ *
+ * Return: Pointer to the array in success and NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc(sizeof(c) * (size + 1));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	s[i] = '\0';
+	return(s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+Malloc and free
+malloc is used to allocate memory in the heap
+and free is used to deallocate memory
+
