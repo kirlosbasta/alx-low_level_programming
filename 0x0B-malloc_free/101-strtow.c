9e405c4a7a01b66a2e32fb69afc305327549ebdb
kirlos basta <kirlosbasta@gmail.com>
2023-10-06 03:59:56 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -79,7 +79,7 @@ int count_words(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] != ' ' && str[i + 1] == ' ') || str[i + 1] == '\0')
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 		{
 			counter++;
 		}
