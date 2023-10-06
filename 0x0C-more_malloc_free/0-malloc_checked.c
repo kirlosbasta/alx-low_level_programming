557e1817ab780d592bfbe8af762eb21866f373fe
kirlos basta <kirlosbasta@gmail.com>
2023-10-07 00:39:36 +0300

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory usinf malloc
+ * @b: The size of the memory
+ *
+ * Return: pointer to the memory in success and exit with code 98 in failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+
+	m = malloc(b);
+	if ( m == NULL)
+	{
+		exit(98);
+	}
+	return (m);
+}
