a021e205488cc7ae3d43950ebb8919b3d4d3dd37
kirlos basta <kirlosbasta@gmail.com>
2023-10-08 20:50:33 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,11 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-		{
-			free(new_ptr);
-			return (NULL);
-		}
+		return (new_ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
