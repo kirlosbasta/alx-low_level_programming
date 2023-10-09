20242c846d9aa912e34c582de316ebfaac71fa23
kirlos basta <kirlosbasta@gmail.com>
2023-10-09 14:18:01 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-main.c
+++ 0x0C-more_malloc_free/100-main.c
@@ -1,7 +1,8 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void *_realloc(void *, unsigned int, unsigned int);
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -12,23 +13,23 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
@@ -36,7 +37,6 @@ void simple_print_buffer(char *buffer, unsigned int size)
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char *p;
@@ -44,8 +44,7 @@ int main(void)
 	unsigned int new_size;
 
 	new_size = 0;
-	p = malloc(sizeof(char) * 30);
-	p = _realloc(p, sizeof(char) * 30, sizeof(char) * new_size);
+	p = _realloc(NULL, sizeof(char) * 30, sizeof(char) * new_size);
 	if (p == NULL)
 	{
 		printf("Failed\n");
