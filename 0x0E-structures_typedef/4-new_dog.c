0b6284e75c1d163596f59887647d1720021e18f4
kirlos basta <kirlosbasta@gmail.com>
2023-10-11 06:45:22 +0300

    Update 5-free_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,17 +13,32 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *cpy_name = name;
-	char *cpy_owner = owner;
-	dog_t *dog = malloc(sizeof(dog_t));
+	dog_t *new_dog;
+	int i;
 
-	if (dog == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = malloc(sizeof(name) + 1);
+	if (new_dog->name == NULL)
 	{
-		free(dog);
+		free(new_dog);
 		return (NULL);
 	}
-	dog->name = cpy_name;
-	dog->age = age;
-	dog->owner = cpy_owner;
-	return (dog);
+	new_dog->owner = malloc(sizeof(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+	new_dog->age = age;
+	for (i = 0; owner[i]; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
+	return (new_dog);
+}
 }
