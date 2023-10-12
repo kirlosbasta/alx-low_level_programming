464fe9dfd06495141b7768acb0929f480b94f44d
kirlos basta <kirlosbasta@gmail.com>
2023-10-12 05:05:20 +0300

    Add Task 3 files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	op = get_op_func(argv[2]);
-	if (op == NULL)
+	if (op == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
