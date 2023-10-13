a4a0b5b262fa2923b63b20ad91f584f83d990afe
kirlos basta <kirlosbasta@gmail.com>
2023-10-13 06:15:03 +0300

    Add 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Print anything
+ * @format: The order of variable to print
+ *
+ * Return: Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	char *s;
+	va_list ap;
+	int i = 0;
+
+	va_start(ap, format);
+	while (*(format + i) != '\0')
+	{
+		switch (*(format + i))
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+						break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+		}
+		if (*(format + i + 1) != '\0')
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
