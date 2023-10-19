ea67745e03f0d967375f1482e8d4f2b816594aa4
kirlos basta <kirlosbasta@gmail.com>
2023-10-19 23:17:46 +0300

    2-add_code.c

---
+++ 0x12-singly_linked_lists/2-add_code.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_code - Add str and it's length to a list
+ * @head: The address of the head
+ * @str: String to be added to the new node
+ *
+ * Return: pointer to the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node = malloc(sizeof(list_t));
+	char *dup_str;
+
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+		return (NULL);
+	node->str = dup_str;
+	node->len = _strlen(dup_str);
+	node->next = *head;
+	*head = node;
+	return (node);
+}
+
+/**
+ * _strlen - count the number of string
+ * @s: pointer to string
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
