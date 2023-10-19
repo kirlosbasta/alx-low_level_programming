b57f16a761530e26a0583510358e1c603760e87d
kirlos basta <kirlosbasta@gmail.com>
2023-10-19 22:46:39 +0300

    Add 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		len++;
+		current = current->next;
+	}
+	return (len);
+}
