ae89e57bc1114e623ff42f870d7d78f0cc21278e
kirlos basta <kirlosbasta@gmail.com>
2023-10-19 22:49:47 +0300

    Add 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * list_len - Return the length of the list
+ * @h: Pointer to the start of the list
+ *
+ * Return: size_t
+ */
+
 size_t list_len(const list_t *h)
 {
 	size_t len = 0;
