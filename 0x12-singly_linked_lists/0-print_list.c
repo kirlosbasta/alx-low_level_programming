88f40cca16342d357bcdfb77bd7404637eadcdf5
kirlos basta <kirlosbasta@gmail.com>
2023-10-19 21:26:40 +0300

    Update 0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,30 @@
 #include "lists.h"
 
+/**
+ * print_list - print a linked list
+ * @h: pointer to the header
+ *
+ * Return: the length of the linked list
+ */
+
 size_t print_list(const list_t *h)
 {
-    const list_t *current = h;
-    size_t counter = 0;
+	const list_t *current = h;
+	size_t counter = 0;
 
-    while (current != NULL)
-    {
-        if (current->str != NULL)
-        {
-            printf("[%u] %s\n", current->len, current->str);
-        }
-        else
-        {
-            printf("[0] (nil)\n");
-        }
-        current = current->next;
-        counter++;
-    }
-    return (counter);
+	while (current != NULL)
+	{
+		if (current->str != NULL)
+		{
+			printf("[%u] %s\n", current->len, current->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		current = current->next;
+		counter++;
+	}
+	return (counter);
 }
+
