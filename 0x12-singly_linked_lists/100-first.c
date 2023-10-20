77c2ef800ab6b65d15fc0950c3cdcbebf0179875
kirlos basta <kirlosbasta@gmail.com>
2023-10-20 03:43:06 +0300

    Add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * premain - A demo of a function executed before main
+ *
+ * Return: Nothing
+ */
+void __attribute__((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,
+	\nI bore my house upon my back!\n");
+}
