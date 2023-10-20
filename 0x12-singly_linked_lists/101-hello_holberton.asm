6514493988a5a3f7cb51ed904ea47e92d260a1a6
kirlos basta <kirlosbasta@gmail.com>
2023-10-20 03:59:23 +0300

    Add hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,4 +1,8 @@
-section .data
+; File: 101-hello_holberton.asm
+; Auth: Kirlos Basta
+; Desc: 64-bit assembly program that prints
+; Hello, Holberton followed by a new line.section .data
+
     hello db "Hello, Holberton", 10, 0
 
 section .text
