0625c3017dc6f047354a0e83001821bdee68438e
kirlos basta <kirlosbasta@gmail.com>
2023-10-23 23:09:41 +0300

    Add 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Remove the first elemnt of a list
+ * @head: Head of the list
+ *
+ * Return: First number of the list
+ */
+
+int pop_listint(listint_t **head)
+{
+	int num;
+	listint_t *tmp;
+
+	if(*head == NULL)
+	{
+		return (0);
+	}
+	tmp = *head;
+	num = tmp->n;
+	*head = (**head).next;
+	free(tmp);
+	return (num);
+}
