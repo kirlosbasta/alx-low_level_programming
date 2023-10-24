6a7b395abfa8cf5f24f03fae5909d5b7cf9d1077
kirlos basta <kirlosbasta@gmail.com>
2023-10-24 04:28:56 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,12 +11,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp = *head;
 
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = (**head).next;
 		free(tmp);
 	}
-	free(*head);
 	*head = NULL;
 }
