e74880f09ff6c4ea5e9984a1ccaf625df052d387
kirlos basta <kirlosbasta@gmail.com>
2023-10-24 04:24:15 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node->n = n;
 	node->next = tmp;
 	*head = node;
-	return (node);	
+	return (node);
 	}
 	for (i = 0; i < idx - 1; i++)
 	{
