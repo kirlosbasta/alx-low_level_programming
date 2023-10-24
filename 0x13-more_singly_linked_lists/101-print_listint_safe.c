a1087074e643c98054a07d19ee8a8a0484a5c699
kirlos basta <kirlosbasta@gmail.com>
2023-10-25 00:56:45 +0300

    Fixes

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print the element of a list
+ * @h: the head of the list
+ *
+ * Return: NUmber of node
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+    size_t num = 0;
+    long int diff;
+
+    while (head)
+    {
+        diff = head - head->next;
+        num++;
+        printf("[%p] %d\n", (void *)head, head->n);
+        if (diff > 0)
+            head = head->next;
+        else
+        {
+            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+            break;
+        }
+    }
+
+    return (num);
+}
