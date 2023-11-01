38abc629851c2c244d56cccaf776e987ba4d13e7
kirlos basta <kirlosbasta@gmail.com>
2023-11-01 02:08:48 +0200

    Add 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 	{
-		if (errno == EEXIST)
-		{
-			open(filename, O_TRUNC);
-			return (-1);
-		}
 		return (-1);
 	}
 	else
