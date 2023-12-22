344ec4aeccc61d3ffc5e4c584d99a95a025a22ff
kirlos basta <kirlosbasta@gmail.com>
2023-12-22 21:01:39 +0200

    Add 4-hash_table_get

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get a value for key
+ * @ht: pointer to the table
+ * @key: Key
+ *
+ * Return: The value or NULL 
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *item;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+	else if (strlen(key) == 0)
+	{
+		return (NULL);
+	}
+	idx = key_index((const unsigned char *)key, ht->size);
+	item = ht->array[idx];
+	if (item == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (item)
+		{
+			if (strcmp(item->key, key) == 0)
+			{
+				return (item->value);
+			}
+			else
+			{
+				item = item->next;
+			}
+		}	
+	}
+	return (NULL);
+}
--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -11,19 +11,33 @@
 int main(void)
 {
     hash_table_t *ht;
-	unsigned long int idx;
+    char *value;
 
     ht = hash_table_create(1024);
-	idx = key_index((const unsigned char *)"betty", ht->size);
-    hash_table_set(ht, "betty", "cool");
-	printf("Key: %s Value: %s\n", ht->array[idx]->key, ht->array[idx]->value);
-	hash_table_set(ht, "betty", "not cool");
-	printf("Key: %s Value: %s\n", ht->array[idx]->key, ht->array[idx]->value);
-	idx = key_index((const unsigned char *)"hetairas", ht->size);
-    hash_table_set(ht, "hetairas", "cool");
-	printf("Key: %s Value: %s\n", ht->array[idx]->key, ht->array[idx]->value);
-	idx = key_index((const unsigned char *)"mentioner", ht->size);
-    hash_table_set(ht, "mentioner", "cool");
-	printf("Key: %s Value: %s\n", ht->array[idx]->key, ht->array[idx]->value);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "c", "isfun");
+
+    value = hash_table_get(ht, "python");
+    printf("%s:%s\n", "python", value);
+    value = hash_table_get(ht, "Bob");
+    printf("%s:%s\n", "Bob", value);
+    value = hash_table_get(ht, "N");
+    printf("%s:%s\n", "N", value);
+    value = hash_table_get(ht, "Asterix");
+    printf("%s:%s\n", "Asterix", value);
+    value = hash_table_get(ht, "Betty");
+    printf("%s:%s\n", "Betty", value);
+    value = hash_table_get(ht, "98");
+    printf("%s:%s\n", "98", value);
+    value = hash_table_get(ht, "c");
+    printf("%s:%s\n", "c", value);
+    value = hash_table_get(ht, "javascript");
+    printf("%s:%s\n", "javascript", value);
     return (EXIT_SUCCESS);
 }
