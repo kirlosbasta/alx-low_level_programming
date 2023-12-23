451ea5adb21780373bf91be652189b991891a11a
kirlos basta <kirlosbasta@gmail.com>
2023-12-23 04:44:22 +0200

    Add 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -194,3 +194,46 @@ void sorted_list(shash_table_t *ht, shash_node_t *item)
 	}
 }
 
+/**
+ * shash_table_get - get a value for key
+ * @ht: pointer to the table
+ * @key: Key
+ *
+ * Return: The value or NULL
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	shash_node_t *item;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+	else if (strlen(key) == 0)
+	{
+		return (NULL);
+	}
+	idx = key_index((const unsigned char *)key, ht->size);
+	item = ht->array[idx];
+	if (item == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (item)
+		{
+			if (strcmp(item->key, key) == 0)
+			{
+				return (item->value);
+			}
+			else
+			{
+				item = item->next;
+			}
+		}
+	}
+	return (NULL);
+}
+
