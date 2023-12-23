2863ebbd4bb24ac4c88413e376e35109088f7cb3
kirlos basta <kirlosbasta@gmail.com>
2023-12-23 04:41:58 +0200

    Add 100-sorted_hash_table.c

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,196 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_create - Create hash table with size @size
+ * @size: Size of hash table
+ *
+ * Return: pointer o the table
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *table = malloc(sizeof(shash_table_t));
+	shash_node_t **arr = malloc(sizeof(shash_node_t *) * size);
+	unsigned long int i;
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	else if (arr == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = NULL;
+	}
+	table->array = arr;
+	table->shead = NULL;
+	table->stail = NULL;
+	return (table);
+}
+
+/**
+ * shash_table_set - Add item to hash table
+ * @ht: Hash table
+ * @key: Key
+ * @value: Value
+ *
+ * Return: 1 in success 0 otherwise
+ */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	shash_node_t *item;
+
+	if (key == NULL || value == NULL || ht == NULL)
+		return (0);
+	if (key[0] == '\0')
+	{
+		return (0);
+	}
+	item = screate_item(key, value);
+	if (item == NULL)
+	{
+		return (0);
+	}
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[idx] == NULL)
+	{
+		ht->array[idx] = item;
+		sorted_list(ht, item);
+		return (1);
+	}
+	else if (ht->array[idx] != NULL)
+	{
+		if (strcmp(key, ht->array[idx]->key) == 0)
+		{
+			free(ht->array[idx]->value);
+			ht->array[idx]->value = item->value;
+			free(item->key);
+			free(item);
+			return (1);
+		}
+		else
+		{
+			sadd_node(&(ht->array[idx]), item);
+			sorted_list(ht, item);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * screate_item - Create new item
+ * @key: Key
+ * @value: Value
+ *
+ * Return: pointer to item or NULL
+ */
+
+shash_node_t *screate_item(const char *key, const char *value)
+{
+	shash_node_t *item;
+	char *key_cpy, *value_cpy;
+
+	item = malloc(sizeof(shash_node_t));
+	if (item == NULL)
+		return (NULL);
+	key_cpy = malloc(sizeof(char) * (strlen(key) + 1));
+	if (key_cpy == NULL)
+	{
+		free(item);
+		return (NULL);
+	}
+	value_cpy = malloc(sizeof(char) * (strlen(value) + 1));
+	if (value_cpy == NULL)
+	{
+		free(item);
+		free(key_cpy);
+		return (NULL);
+	}
+	key_cpy = strcpy(key_cpy, key);
+	value_cpy = strcpy(value_cpy, value);
+	item->key = key_cpy;
+	item->value = value_cpy;
+	item->next = NULL;
+	item->snext = NULL;
+	item->sprev = NULL;
+	return (item);
+}
+
+/**
+ * sadd_node - add node to front of linked list
+ * @head: Head of the list
+ * @item: item to add
+ *
+ * Return: Nothing
+ */
+
+void sadd_node(shash_node_t **head, shash_node_t *item)
+{
+	if (head == NULL)
+	{
+		*head = item;
+	}
+	else
+	{
+		item->next = *head;
+		*head = item;
+	}
+}
+
+/**
+ * sorted_list - Add @item to the sorted position according to ASCII
+ * @ht: Hash table
+ * @item: Item
+ *
+ * Return:Nothing
+ */
+void sorted_list(shash_table_t *ht, shash_node_t *item)
+{
+	shash_node_t *current;
+
+	if (ht->shead == NULL)
+	{
+		ht->shead = item;
+		ht->stail = item;
+		item->snext = NULL;
+		item->sprev = NULL;
+	}
+	else
+	{
+		current = ht->shead;
+		while (current->snext)
+		{
+			if (strcmp(item->key, current->key) <= 0)
+				break;
+			current = current->snext;
+		}
+		if (current->sprev == NULL && strcmp(item->key, current->key) <= 0)
+		{
+			item->snext = current;
+			item->sprev = NULL;
+			current->sprev = item;
+			ht->shead = item;
+		}
+		else if (current->snext == NULL && strcmp(item->key, current->key) <= 0)
+		{
+			item->snext = NULL;
+			item->sprev = current;
+			current->snext = item;
+			ht->stail = item;
+		}
+		else
+		{
+			item->snext = current;
+			item->sprev = current->sprev;
+			current->sprev = item;
+			item->sprev->snext = item;
+		}
+	}
+}
+
