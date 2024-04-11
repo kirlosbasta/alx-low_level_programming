0bb38431f0c71b706ecdd0924828baea5c3131ec
kirlos basta <kirlosbasta@gmail.com>
2024-04-11 02:33:25 +0200

    Add 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -32,8 +32,6 @@ int jump_search(int *array, size_t size, int value)
 		{
 			start = end;
 			end += step;
-			if (start >= (int)size)
-				return (-1);
 		}
 	}
 	printf("Value found between indexes [%d] and [%d]\n", start, end);
